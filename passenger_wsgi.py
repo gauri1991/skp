a6118cb4fcd7385060117408f98cb23dd9ec863f
Gauri <gauri@epochserve.com>
2025-09-22 14:50:51 +0530

    Fix passenger_wsgi.py recursion error - remove imp module and properly configure Django WSGI

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 WSGI config for cPanel deployment using Phusion Passenger.
 This file is required for deploying Django on cPanel with Python apps support.
@@ -6,14 +7,19 @@ This file is required for deploying Django on cPanel with Python apps support.
 import os
 import sys
 
-# Add your project directory to the sys.path
+# Set the project path
 project_home = '/home/meenvstf/public_html/sumithrakp'
+
+# Add project to Python path
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Set environment variable for Django settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'sumithrakp_website.settings'
+# Change to project directory
+os.chdir(project_home)
 
-# Import and configure Django
+# Set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sumithrakp_website.settings')
+
+# Import Django WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
